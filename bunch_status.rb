b7c2fb896a89f09ac356c7d5eb506c55ff47a615
Brett Terpstra <me@brettterpstra.com>
2021-07-20 13:06:27 -0500

    New commands, script consolidation
    - Incorporate create_bunch_buttons into btt_stats.rb
    - uuids command to convert copied widget data into config settings for
    refresh command
    - "add bunch" command to add a Bunch group and buttons for installed
    Bunches
    - Make refresh command keypaths case insensitive and allow omission of spaces

--- bunch_status.rb
+++ bunch_status.rb
@@ -5,13 +5,8 @@ require 'json'
 open_color = '165,218,120,255'
 closed_color = '95,106,129,255'
 
-# bunch_folder = `/usr/bin/osascript -e 'tell app "Bunch" to get preference "folder"'`.strip
-# status_file = File.join(bunch_folder,'bunch_status.json')
-# status_file = '/Users/ttscoff/bunch_status.json'
-
-# status = JSON.parse(IO.read(status_file))
 open_bunches = `/usr/bin/osascript -e 'tell app "Bunch" to list open bunches'`.strip.downcase.split(/, /)
-bunch = ARGV[0].strip.downcase
+bunch = ARGV.join(' ').strip.downcase
 
 color = open_bunches.include?(bunch.downcase) ? open_color : closed_color
 
