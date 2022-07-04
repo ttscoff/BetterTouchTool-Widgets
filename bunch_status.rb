9e7bc5e8b4f40b219579566e44d15e87ad9e9af0
Brett Terpstra <me@brettterpstra.com>
2022-07-04 05:25:49 -0500

    Stream Deck additions
    - "btt_scripts.rb add streamdeck ..." capabilties
    - return stream deck background color from most widgets
    - add "audio status mute/input vol/output vol" functions
    - add "audio mute/unmute/volumeup/volumedown" functions
    - Make default bunch status script better handle Bunch Beta
    - "btt_scripts.rb add streamdeck bunch" creates a group of all available
    bunches on stream deck
    - Fix doing status uncoloring
    - uuids command can read Stream Deck commands and groups
    - cpu command has been modified to work with a 10-core Mac Studio which
    I'm pretty sure will break it everywhere else. I don't know why my
    Studio results are so weird...

--- bunch_status.rb
+++ bunch_status.rb
@@ -2,12 +2,46 @@
 # frozen_string_literal: true
 
 require 'json'
+
+# Usage in a BetterTouchTool Shell Script Widget
+#
+# For a Stream Deck widget:
+# /path/to/bunch_status.rb sd title "Bunch Name"
+#
+# For a Touch Bar widget:
+# /path/to/bunch_status.rb "Bunch Name"
+
+# Define open and closed background colors
 open_color = '165,218,120,255'
 closed_color = '95,106,129,255'
 
-open_bunches = `/usr/bin/osascript -e 'tell app "Bunch" to list open bunches'`.strip.downcase.split(/, /)
+# To return Stream Deck button background, use "sd" as the first argument
+
+if ARGV[0] =~ /(sd|stream(deck)?)/i
+  streamdeck = true
+  ARGV.shift
+end
+
+# If the next argument is "title", text with the Bunch name
+# will be returned in addition to the background color. If
+# not, only a background color is returned.
+
+if ARGV[0] =~ /title/
+  title = true
+  ARGV.shift
+end
+
+bunch = `ps ax | grep -v grep | grep -c 'Bunch Beta'`.strip.to_i == 1 ? 'Bunch Beta' : 'Bunch'
+
+warn "Targeting #{bunch}"
+
+open_bunches = `/usr/bin/osascript -e 'tell app "#{bunch}" to list open bunches'`.strip.downcase.split(/, /)
 bunch = ARGV.join(' ').strip.downcase
 
 color = open_bunches.include?(bunch.downcase) ? open_color : closed_color
 
-print "{\"background_color\":\"#{color}\"}"
+if streamdeck
+  print "{ \"text\":\"#{title ? bunch : ''}\",\"BTTStreamDeckBackgroundColor\": \"#{color}\"}"
+else
+  print "{\"background_color\":\"#{color}\"}"
+end
